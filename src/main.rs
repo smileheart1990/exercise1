f3e0c1add36c6e5c441edb1e02bb9f1abe9ad062
Sirius <smileheart1990@gmial>
2021-07-27 00:53:25 +0800

    NA

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-
+/*
 mod test2;
 mod huobi_helper;
 mod twitter_listener_service;
 use std::thread::{sleep};
 use std::time::Duration;
+use futures::executor::block_on;
 fn main(){
     /*let price = huobi_helper::huobi_help::get_latest_trade_price();
     match price {
@@ -16,7 +17,26 @@ fn main(){
         Err(error)=>{println!("error is {}", error)}
     }*/
 
-    let successful_or_not = twitter_listener_service::twitter_listner::start_twitter_listner_services();
+    block_on(twitter_listener_service::twitter_listner::start_twitter_listner_services());
+
+
 
     sleep(Duration::from_millis(100000));
+}*/
+
+use futures::prelude::*;
+use twitter_stream::{Token, TwitterStream};
+
+#[tokio::main]
+async fn main() {
+    println!("hi");
+    let token = Token::from_parts("yex95KUGuZi1Et7AwQNMIpooW", "DOtMcrVCSbUyVDFB7nKepUKWR2hrT1XGUFVkJNKegU25Cb71JX", "1379464579705618521-w9rVTAsbdTJLG5ENnIGvAPog72wRPQ", "AxBjSzE1wPIVWcGqS6BASJ0qdWPTjzOJcswK4QP5R1Lg5");
+    TwitterStream::track("@Twitter", &token)
+        .try_flatten_stream()
+        .try_for_each(|json| {
+            println!("{}", json);
+            future::ok(())
+        })
+        .await
+        .unwrap();
 }
--- src/twitter_listener_service.rs
+++ src/twitter_listener_service.rs
@@ -7,6 +7,7 @@ pub struct twitter_listner {}
 impl twitter_listner {
     pub async fn start_twitter_listner_services() {
         // need to replace the key or token, or config that in the file.
+        println!("hi");
         let token = Token::from_parts("yex95KUGuZi1Et7AwQNMIpooW", "DOtMcrVCSbUyVDFB7nKepUKWR2hrT1XGUFVkJNKegU25Cb71JX", "1379464579705618521-w9rVTAsbdTJLG5ENnIGvAPog72wRPQ", "AxBjSzE1wPIVWcGqS6BASJ0qdWPTjzOJcswK4QP5R1Lg5");
         TwitterStream::track("@Twitter", &token)
             .try_flatten_stream()
@@ -17,6 +18,5 @@ impl twitter_listner {
             .await
             .unwrap();
         println!("the twitter listen services up")
-
     }
 }
