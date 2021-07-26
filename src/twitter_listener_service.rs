ad107d7ba2dc6003646b2bba1eb6f734ba2383cb
Sirius <smileheart1990@gmial>
2021-07-26 22:41:55 +0800

    NA

--- src/twitter_listener_service.rs
+++ src/twitter_listener_service.rs
@@ -8,7 +8,7 @@ impl twitter_listner {
     pub async fn start_twitter_listner_services() {
         // need to replace the key or token, or config that in the file.
         let token = Token::from_parts("yex95KUGuZi1Et7AwQNMIpooW", "DOtMcrVCSbUyVDFB7nKepUKWR2hrT1XGUFVkJNKegU25Cb71JX", "1379464579705618521-w9rVTAsbdTJLG5ENnIGvAPog72wRPQ", "AxBjSzE1wPIVWcGqS6BASJ0qdWPTjzOJcswK4QP5R1Lg5");
-        TwitterStream::track("https://stream.twitter.com/1.1/statuses/filter.json?track=hi", &token)
+        TwitterStream::track("@Twitter", &token)
             .try_flatten_stream()
             .try_for_each(|json| {
                 println!("{}", json);
